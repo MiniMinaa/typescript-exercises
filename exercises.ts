c5dbd8df7b3c180fba41f25fcd7226c8273c5734
Mina R <minarumang@icloud.com>
2026-01-28 15:26:11 +0100

    union types ex2

--- exercises.ts
+++ exercises.ts
@@ -1,6 +1,16 @@
-type IDType = number | string
+//union types ex1
+type IDType = number | string //OR, this is a union type meaning a value can have more than 1 possible type
 
 const showID = (code: IDType) => {
     console.log("Your ID is:", code)
 }
 showID(123);
+//showID(abc);
+
+//ex2
+type Fruit = "apple" | "banana" | "orange"; //this is union of string literals, value can only be one of these 3
+
+const eatFruit = (Fruit: Fruit) => { // (fruit: fruit) is parameter list
+    console.log("You ate an..." + Fruit);
+};
+eatFruit("apple");
